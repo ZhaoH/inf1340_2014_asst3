30ce37bfdeb4132205177526447a8806b6057210
ohjchristine <christine.jihae.oh@gmail.com>
2014-11-23 23:09:45 -0500

    Import json files

--- mining.py
+++ mining.py
@@ -10,11 +10,25 @@ __license__ = "MIT License"
 
 __status__ = "Prototype"
 
+
+"""
+    1. Read json file
+    2. Required fields: date, volume, close price
+    3. How many data points are there in the JSON file for that month (import datetime - Sasa used it)
+    4. Calculate monthly average
+    5. In tuple - put calculated monthly average with the month and year
+    6. Append tuple to a list - monthly_averages_list
+"""
+
 # imports one per line
 import json
 import datetime
 
-#import json
+
+
+
+
+REQUIRED_FIELDS = ["Date", "Close", "Volume"]
 
 stock_data = []
 monthly_averages = []
@@ -27,19 +41,27 @@ def read_stock_data(stock_name, stock_file_name):
     :param stock_file_name: The name of a JSON formatted file that contains stock data
     :return: Tuple. Consist of the average for that month, and the date (month and year)
     """
+
+    with open(stock_file_name) as file_handle:
+        file_contents = file_handle.read()
+        json.loads(file_contents)
+
+
+
+
+
+
     return
 
-#REQUIRED_FIELDS = ["date", "close", "volume"]
-#average price = (V1  C1 + V2  C2)=(V1 + V2)
-#monthly_averages_list: tuple of averages for each month
 
-#from last assignment:
-#def valid_date_format(date_string):
-#    """
-#    Checks whether a date has the format YYYY-mm-dd in numbers
-#    :param date_string: date to be checked
-#   :return: Boolean True if the format is valid, False otherwise
-#    """
+
+def average_per_month(date_string):
+    """
+    Identifies the month and year from format YYYY-MM-DD
+    :param date_string: date to be checked
+    :return: tuple with monthly average, month, and year
+    """
+
 #    try:
 #        datetime.datetime.strptime(date_string, '%Y-%m-%d')
 #        return True
@@ -54,9 +76,5 @@ def six_worst_months():
     return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
 
 
-def read_json_from_file(file_name):
-    with open(file_name) as file_handle:
-        file_contents = file_handle.read()
 
-    return json.loads(file_contents)
 
