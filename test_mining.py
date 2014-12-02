50dec12eb94ec7302adb1de1d649be2513f70adc
ZhaoH <Zhao.hu@outlook.com>
2014-12-01 21:56:53 -0500

    Add one more test case

--- test_mining.py
+++ test_mining.py
@@ -24,6 +24,13 @@ def test_goog():
 
 def test_tseso():
     read_stock_data("TSE-SO", "data/TSE-SO.json")
+    assert six_best_months() == [('2007/12', 20.98), ('2007/11', 20.89), ('2013/05', 19.96), ('2013/06', 19.94),
+                                 ('2013/04', 19.65), ('2007/10', 19.11)]
+    assert six_worst_months() == [('2009/03', 1.74), ('2008/11', 2.08), ('2008/12', 2.25), ('2009/02', 2.41),
+                                  ('2009/04', 2.75), ('2009/01', 3.14)]
+
+def test_tseso1():
+    read_stock_data("TSE-SO", "data/TSE-SO1.json")
     assert six_best_months() == "There is not enough data"
     assert six_worst_months() == "There is not enough data"
 
